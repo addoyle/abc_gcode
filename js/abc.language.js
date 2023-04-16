b8c10ac3d469bdd45e8394f225162e0fc3ddb9ff
adoyle <andy.d.doyle@gmail.com>
2023-04-16 14:44:31 -0400

    Initial page with some ABC syntax highlighting logic

---
+++ js/abc.language.js
@@ -0,0 +1,29 @@
+Prism.languages.abc = {
+  property: {
+    pattern: /[A-Z](?=\:)/,
+    lookbehind: true,
+    greedy: true
+  },
+  string: {
+    pattern: /(^|[^\\])"(?:\\.|[^\\"\r\n])*"(?!\s*:)/,
+    lookbehind: true,
+    greedy: true
+  },
+  comment: {
+    pattern: /\%.*/,
+    greedy: true
+  },
+  keyword: {
+    pattern: /(?<=[A-G])[\d,'\/]\d*|[<>]*[\^_]?[~^_=\.vu<>](?=[A-G])/i,
+    lookbehind: true,
+    greedy: true
+  },
+  number: /\d/i,
+  operator: /[:\|\\[\](){}-]/,
+  boolean: {
+    pattern: /((?:[A-Z])\d)/,
+    lookbehind: true
+  }
+};
+
+Prism.languages.webmanifest = Prism.languages.abc;
